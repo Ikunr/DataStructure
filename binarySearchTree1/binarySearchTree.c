454bea187321ed0c88d806a43a715a26104a4034
shuo <111@example.com>
2023-12-25 15:22:40 +0800

    [新增]完善树的初始化

--- binarySearchTree1/binarySearchTree.c
+++ binarySearchTree1/binarySearchTree.c
@@ -1,5 +1,15 @@
 #include "binarySearchTree.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/* 状态码 */
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+};
 
 
 
@@ -7,7 +17,39 @@
 int binarySearchTreeInit(BinarySearchTree **pBstree)
 {
     int ret = 0;
-    
+    BinarySearchTree *BSTree = (BinarySearchTree *)malloc(sizeof(BSTreeNode) * 1);
+    if(!BSTree)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 清楚脏数据*/
+    memset(BSTree, 0, sizeof(BSTreeNode ) * 1);
+    /* 初始化树 */
+    {
+        BSTree->root = NULL;
+        BSTree->size = 0;
+    }
+
+#if 1
+    /* 分配根节点 */
+    BSTree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if(!BSTree->root)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 清除脏数据 */
+    memset(BSTree->root, 0, sizeof(BSTreeNode) * 1);
+
+#endif
+    {
+        BSTree->root->left = NULL;
+        BSTree->root->right = NULL;
+        BSTree->root->parent = NULL;
+        BSTree->root->data = 0;
+    }
+
+    *pBstree = BSTree; 
     return ret;
 }
 
